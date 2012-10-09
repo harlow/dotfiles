6c5cffaf6559d647a230b8b9c6eb593b50e05fe5
Harlow Ward <harlow@thoughtbot.com>
2012-10-09 16:31:05 -0400

    Need expanded path call on directory name

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -15,6 +15,7 @@ symlinks_created = []
 
 Dir.foreach('.') do |item|
   target = "#{ENV['HOME']}/.#{item}"
+  path = File.expand_path(File.dirname(File.dirname(__FILE__)))
   next if ignored_files.include? item
   next if File.symlink? target
 
@@ -22,7 +23,7 @@ Dir.foreach('.') do |item|
     puts "WARNING: #{item} exists but is not a symlnk"
   else
     symlinks_created << item
-    `ln -s #{item} #{target}`
+    `ln -s #{path}/#{item} #{target}`
   end
 end
 
