3347c837e23cfe9e4d5bf1df46c9bb23adcae5bc
Harlow Ward <harlow@thoughtbot.com>
2012-10-10 08:28:20 -0400

    wip: workspace

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -27,9 +27,14 @@ Dir.foreach('.') do |item|
   end
 end
 
-unless File.symlink? '/user/local/bin/run_command'
+unless File.symlink? '/usr/local/bin/run_command'
   path = File.expand_path(File.dirname(File.dirname(__FILE__)))
   `ln -s #{path}/misc/run_command /usr/local/bin/run_command`
+end
+
+unless File.symlink? '/usr/local/bin/workspace'
+  path = File.expand_path(File.dirname(File.dirname(__FILE__)))
+  `ln -s #{path}/misc/workspace /usr/local/bin/workspace`
 end
 
 puts "#{symlinks_created.count} symlinks created"
