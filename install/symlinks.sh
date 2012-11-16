ba0b853b78cda35104a9ef3dfd72b71955d99732
Harlow Ward <harlow@thoughtbot.com>
2012-11-16 10:07:51 -0500

    Added comments to install script

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env ruby
 require 'fileutils'
 
-ignored_files = [
+IGNORED_FILES = [
   '.',
   '..',
   '.gitignore',
   '.git',
+  'bin',
   'install',
-  'README.md',
-  'bin'
+  'README.md'
 ]
 
-symlink_count = 0
+puts 'Creating symlinks'
+puts '------------------------------'
 
+# install symlinks for dotfiles to ~/.file_name
 Dir.foreach('.') do |item|
   target = "#{ENV['HOME']}/.#{item}"
-  path = File.expand_path(File.dirname(File.dirname(__FILE__)))
-
-  next if ignored_files.include? item
+  current_path = File.expand_path(File.dirname(File.dirname(__FILE__)))
+  next if IGNORED_FILES.include? item
   next if File.symlink? target
 
   if File.exists? target
@@ -25,19 +26,22 @@ Dir.foreach('.') do |item|
     next
   end
 
-  symlink_count = symlink_count + 1
-  `ln -s #{path}/#{item} #{target}`
+  puts " + #{target}"
+  `ln -s #{current_path}/#{item} #{target}`
 end
 
+# install symlinks to scripts in /usr/local/bin/file_name
 Dir.foreach('bin') do |item|
   target = "/usr/local/bin/#{item}"
-  path = File.expand_path(File.dirname(File.dirname(__FILE__)))
-
-  next if item == '.' or item == '..'
+  current_path = File.expand_path(File.dirname(File.dirname(__FILE__)))
+  next if IGNORED_FILES.include? item
   next if File.symlink? target
 
-  symlink_count = symlink_count + 1
-  `ln -s #{path}/bin/#{item} #{target}`
-end
+  if File.exists? target
+    puts "WARNING: #{item} exists but is not a symlnk"
+    next
+  end
 
-puts "#{symlink_count} symlinks created"
+  puts " + #{target}"
+  `ln -s #{current_path}/bin/#{item} #{target}`
+end
