546e123661d06e4998119bb8e5a7a0a04449c25e
Harlow Ward <harlow@thoughtbot.com>
2013-02-07 09:21:04 -0800

    Fast Prompt

--- zsh/config/prompt.zsh
+++ zsh/config/prompt.zsh
@@ -4,17 +4,10 @@ setopt prompt_subst
 zstyle ':vcs_info:*' stagedstr '%F{green}+'
 zstyle ':vcs_info:*' unstagedstr '%F{red}!'
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{11}%r'
 zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' formats '(%F{green}%b%c%u%F{white})'
 
 precmd () {
-  if [ -d "$PWD/.git" ]; then
-    if [[ -z $(git ls-files --other --exclude-standard 2> /dev/null) ]] {
-      zstyle ':vcs_info:*' formats '(%F{green}%b%c%u%F{white})'
-    } else {
-      zstyle ':vcs_info:*' formats '(%F{green}%b%c%u%F{red}?%F{white})'
-    }
-  fi
   vcs_info
 }
 
