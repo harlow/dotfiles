59b8fe8bc7f289cebc26d40ebf35929107ace48d
Harlow Ward <harlow@thoughtbot.com>
2013-02-28 16:48:34 -0800

    make prompt output grey

--- zsh/config/prompt.zsh
+++ zsh/config/prompt.zsh
@@ -1,14 +1,14 @@
 autoload -Uz vcs_info
 setopt prompt_subst
 
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' formats '(%F{green}%b%c%u%F{grey})'
 zstyle ':vcs_info:*' stagedstr '%F{green}+'
 zstyle ':vcs_info:*' unstagedstr '%F{red}!'
-zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:*' formats '(%F{green}%b%c%u%F{white})'
 
 precmd () {
   vcs_info
 }
 
-PROMPT='%3~${vcs_info_msg_0_} %F{yellow}$%F{white} '
+PROMPT='%F{grey}%3~${vcs_info_msg_0_} %F{yellow}$%F{grey} '
